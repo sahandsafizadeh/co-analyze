d6ac26e16d2b9184392a3e9f544472303dcdb652
sahand.safizadeh <sahandsafizadeh.s@gmail.com>
2021-09-17 10:23:43 +0430

    application loader and error handler component implemented

---
+++ Front/co-analyze/src/components/AppLoader.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="root">
+    <div v-if="error" id="error" class="content">Error :(</div>
+    <div v-else id="loader" class="content">{{ content }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Boolean,
+      required: true,
+    },
+  },
+
+  data() {
+    return {
+      CONTENT_CHANGE_TIMEOUT_MS: 500,
+      FULL_CONTENT: '...',
+      content: '',
+    };
+  },
+
+  created() {
+    setInterval(() => {
+      if (this.content === this.FULL_CONTENT)
+        this.content = '';
+      else
+        this.content += '.';
+    }, this.CONTENT_CHANGE_TIMEOUT_MS);
+  },
+};
+</script>
+
+<style scoped>
+.root {
+  z-index: 2;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.85);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.content {
+  text-align: center;
+  font-weight: bold;
+  font-size: 6rem;
+}
+
+#error {
+  color: #ff0000;
+}
+
+#loader {
+  color: #ffffff;
+}
+</style>
